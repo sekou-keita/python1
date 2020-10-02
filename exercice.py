01753028455bbfdd56cfa1de531fd72dd8e3ab79
Sekou KEITA <you@example.com>
2020-10-02 19:32:46 +0000

    derniere modification:python

--- exercice.py
+++ exercice.py
@@ -3,19 +3,19 @@ def eleve_info():
         fichier = open("eleve.txt","a")
         Nom = input("Taper votre Nom: ")
         Prenom = input("Taper votre Prenom: ")
-        age = input("Taper votre age: ")
+        date = input("Taper votre date: ")
         classe = input("Taper votre classe: ")
-        fichier.writelines(f"{Nom},{Prenom},{age} an,{classe}Annee\n")
+        fichier.writelines(f"{Nom},{Prenom},{date},{classe} Annee\n")
         print(fichier)
         fichier.close
 def lire_fichier():
     fichier = open("eleve.txt","r")
     contenue = fichier.readlines()
     for eleve in contenue:
-        code = eleve.split(",")
-        print(f"Nom de l'eleve: {code[0]}\nprenom:{code[1]}\nage de l'eleve: {code[2]}\nclasse: {code[3]}\n")
-    fichier.close()    
-    
-lire_fichier()
-eleve_info()   
+        nom, prenom, date, classe, = eleve.split(",")
+        mois = date.split("/")
+        print(f"Nom et Prenom de l'eleve: {nom} {prenom}\nDate de naissance est:{date}\nmoi de naissance est:{mois[1]} mois\nclasse:{classe}")
+lire_fichier() 
+eleve_info()
+
                 
